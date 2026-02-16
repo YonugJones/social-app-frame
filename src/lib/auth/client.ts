43ac103136a972bbc043f9e9d096e327f6d8df19
Peter Kerfoot <peterckerfoot@gmail.com>
2026-02-15 18:52:21 -0600

    bug: switched to production domain for db

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -3,6 +3,4 @@
 
 import { createAuthClient } from 'better-auth/client'
 
-export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3000',
-})
+export const authClient = createAuthClient()
